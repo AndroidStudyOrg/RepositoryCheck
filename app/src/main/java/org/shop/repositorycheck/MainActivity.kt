3d26152818a367d05d6ecd3ebe110877adc70243
wbham <wbham97@naver.com>
2023-12-30 00:43:39 +0900

    [Feat] - Retrofit으로 유저 검색하기
    - 유저 검색

--- app/src/main/java/org/shop/repositorycheck/MainActivity.kt
+++ app/src/main/java/org/shop/repositorycheck/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import org.shop.repositorycheck.databinding.ActivityMainBinding
 import org.shop.repositorycheck.model.Repo
+import org.shop.repositorycheck.model.UserDto
 import org.shop.repositorycheck.network.GithubService
 import retrofit2.Call
 import retrofit2.Callback
@@ -21,17 +22,28 @@ class MainActivity : AppCompatActivity() {
         }
 
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.github.com/")
+            .baseUrl("https://api.github.com")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
         val githubService = retrofit.create(GithubService::class.java)
-        githubService.listRepos("square").enqueue(object : Callback<List<Repo>> {
-            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
-                Log.e("MainActivity", response.body().toString())
+        githubService.listRepos(resources.getString(R.string.github_token), "square")
+            .enqueue(object : Callback<List<Repo>> {
+                override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+                    Log.e("MainActivity", "List Repo: ${response.body().toString()}")
+                }
+
+                override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+
+                }
+            })
+
+        githubService.searchUsers("squar").enqueue(object : Callback<UserDto> {
+            override fun onResponse(call: Call<UserDto>, response: Response<UserDto>) {
+                Log.e("MainActivity", "Search User: ${response.body().toString()}")
             }
 
-            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+            override fun onFailure(call: Call<UserDto>, t: Throwable) {
 
             }
         })
--- app/src/main/java/org/shop/repositorycheck/network/GithubService.kt
+++ app/src/main/java/org/shop/repositorycheck/network/GithubService.kt
@@ -1,11 +1,20 @@
 package org.shop.repositorycheck.network
 
 import org.shop.repositorycheck.model.Repo
+import org.shop.repositorycheck.model.UserDto
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface GithubService {
     @GET("/users/{username}/repos")
-    fun listRepos(@Path("username") username: String): Call<List<Repo>>
+    fun listRepos(
+        @Header("Authorization") token: String,
+        @Path("username") username: String
+    ): Call<List<Repo>>
+
+    @GET("/search/users")
+    fun searchUsers(@Query("q") query: String): Call<UserDto>
 }
